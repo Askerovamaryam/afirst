3b5761db3f29734f876d969bcbd5df3a3a927e84
Maryam Askerova <askerovamaryam107@gmail.com>
2024-09-25 13:38:01 +0400

    book

---
+++ 25.09/25.09.10.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <map>
+#include <string>
+int main()
+{
+	std::map<std::string,int>data;
+	std::string key;
+	int value;
+	while(std::cin>>key>>value)
+	{
+		data[key]=value;
+	}
+	data.erase("hello");
+	if(auto iter=data.find("test");iter!=data.end())
+	{
+		std::cout<<"Found the key" <<iter->first<< "With the value" <<iter->second << "\n";
+	}
+	else{
+		std::cout<<"not found\n";
+	}
+	return 0;
+}
+		
+		
---
+++ 25.09/25.09.3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <list>
+int main()
+{
+	std::list<int> l{10,15,20};
+	auto iter=l.begin();
+	std::cout<< *iter <<"\n";
+	++iter;
+	std::cout<< *iter<<"\n";
+	--iter;
+	std::cout<< *iter<<"\n";
+	return 0;
+}
+	
